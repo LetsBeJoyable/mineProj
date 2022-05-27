78464a7b020e7ed06ef18122df105cc05cd0a4f3
letsbejoyable <mrconsultoria123@gmail.com>
2022-05-27 14:37:23 -0300

    Primeiro commit - Arquivos iniciais adicionados.

---
+++ src/main/java/me/joyable/mineProj.java
@@ -0,0 +1,13 @@
+package me.joyable;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class mineProj extends JavaPlugin {
+
+    public void onLoad(){}
+
+    public void onEnable(){}
+
+    public void onDisable(){}
+
+}
